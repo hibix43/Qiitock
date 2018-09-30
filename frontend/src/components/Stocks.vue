8fca0b8e480d810e7f779acf9c141f895026cc0f
res0nanz <b1600661@planet.kanazawa-it.ac.jp>
2018-09-30 23:36:17 +0900

    add searching with title and tags.

--- frontend/src/components/Stocks.vue
+++ frontend/src/components/Stocks.vue
@@ -1,21 +1,21 @@
 <template>
   <div id="stocks" v-if="stocks !== null">
     <h1>Stocks!</h1>
-    <div v-for="stock in stocks" v-bind:key="stock.id">
+    <input type="text" v-model="searchWords" placeholder="Search title or tag.."/>
+    <div v-for="stock in filteredStocks" v-bind:key="stock.id">
       <!--td?-->
-      <a v-bind:href="stock.url">{{ stock.title }}</a>
-      <ul>
-        <li v-for="(tag_name, index) in stock.tags" v-bind:key="index">
-          {{ tag_name }}
-        </li>
-      </ul>
+      <h2>{{ stock.title }}</h2>
+      <a v-bind:href="stock.url" target="_blank">Qiitaで読む</a>
+      <div v-for="(tagName, index) in stock.tags" v-bind:key="index">
+        <button v-on:click="searchStocksByTag(tagName)">{{ tagName }}</button>
+      </div>
       <button v-on:click="toggleAgenda(stock)">Agenda</button>
-      <div v-show="stock.show_agenda">
+      <div v-show="stock.showAgenda">
         <span v-html="stock.agenda"></span>
       </div>
     </div>
     <div v-if="pageCounter !== null">
-      <button v-on:click="getMore">もっとみる</button>
+      <button v-on:click="getMoreStocks">もっとみる</button>
     </div>
   </div>
 </template>
@@ -27,7 +27,19 @@ export default {
   data: function () {
     return {
       stocks: [],
-      pageCounter: 1
+      pageCounter: 1,
+      searchWords: ''
+    }
+  },
+  computed: {
+    filteredStocks: function () {
+      // 検索指定がなければすべて
+      if (this.searchWords === '') {
+        return this.stocks
+      } else {
+        // 検索
+        return this.searchStocks()
+      }
     }
   },
   methods: {
@@ -43,8 +55,9 @@ export default {
                 // JSONデータを配列に格納
                 vm.$set(vm.stocks, index, response.data[i])
                 // 目次の表示トグル
-                vm.$set(vm.stocks[index], 'show_agenda', false)
+                vm.$set(vm.stocks[index], 'showAgenda', false)
               }
+              // これ以上のストックは存在しない
               if (response.data.length < 20) {
                 vm.pageCounter = null
               }
@@ -54,7 +67,7 @@ export default {
           }
         )
     },
-    getMore: function () {
+    getMoreStocks: function () {
       let vm = this
       if (vm.pageCounter !== null) {
         vm.pageCounter++
@@ -62,7 +75,37 @@ export default {
       }
     },
     toggleAgenda: function (stock) {
-      stock.show_agenda = !stock.show_agenda
+      stock.showAgenda = !stock.showAgenda
+    },
+    searchStocks: function () {
+      // 検索結果
+      let stocks = []
+      // 検索ワードを整形
+      let searchWords = this.arangeSearchWords()
+      // 各投稿に関して
+      for (let s of this.stocks) {
+        // 検索ワードが含まれるか
+        for (let word of searchWords) {
+          if (s.title.toLowerCase().indexOf(word) !== -1 ||
+              s.tags.some(tag => tag.toLowerCase().indexOf(word) !== -1)) {
+            // ヒット
+            stocks.push(s)
+          }
+        }
+      }
+      console.log(stocks)
+      return stocks
+    },
+    arangeSearchWords: function () {
+      let words = []
+      // 大文字・小文字両方
+      for (let word of this.searchWords.split(' ')) {
+        words.push(word.toLowerCase())
+      }
+      return words
+    },
+    searchStocksByTag: function (tag) {
+      this.searchWords = tag
     }
   },
   created: function () {
