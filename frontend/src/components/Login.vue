bda0478105dfde6ab9434b5131708635fd8f2f6c
res0nanz <b1600661@planet.kanazawa-it.ac.jp>
2018-10-03 01:02:50 +0900

    add design in login-index.

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,7 +1,16 @@
 <template>
   <div id="login" v-if="login_url">
-    <h1>Login!</h1>
-    <a v-bind:href="login_url">ログイン・認証</a>
+    <h1>Welcome to Qiitock !</h1>
+    <a v-bind:href="login_url" class="button">Qiitaアカウントにログインする</a>
+    <div class="description">
+      <h2>Qiitockとは</h2>
+      <p>プログラマのための問題共有サイト<a href="https://qiita.com">Qiita</a></p>
+      <p>数ある投稿のなかから、あなたが</p>
+      <p>役立つ記事だ。もう一度、読むはずだ。</p>
+      <p>そう感じてストックした良質な記事。</p>
+      <p>Qiitockは、そんなあなたのストックを</p>
+      <p>振り返りやすくするWebサービスです。</p>
+    </div>
   </div>
 </template>
 
@@ -40,11 +49,41 @@ export default {
 
 <style>
 #login {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
+  width: 100%;
+  padding: 0;
+  padding-top: 64px;
+}
+#login h1 {
+  margin-bottom: 64px;
+  font-size: 3rem;
+  color: #59ABE3;
+}
+#login .button {
+  padding: 16px 64px;
+  background: #59ABE3;
+  border: 2px solid #59ABE3;
+  border-radius: 4px;
+}
+#login > a, #login > a:hover, #login > a:visited {
+  text-decoration: none;
+  font-weight: bold;
+  color: #FFFFFF;
+}
+#login .description {
+  text-align: center;
+  padding: 32px;
   color: #2c3e50;
-  margin-top: 60px;
+}
+#login .description a,
+#login .description a:hover,
+#login .description a:visited {
+  text-decoration: none;
+  font-weight: bold;
+  color: #59ABE3;
+}
+
+#login .description p {
+  margin: 16px 0;
 }
 </style>
